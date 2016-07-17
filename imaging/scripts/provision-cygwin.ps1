d21853af0e8aeacd85f4f85518bdbbd4d1cddf01
deluxebrain <matt.caton@mac.com>
2016-07-17 15:58:27 +0100

    Working on cygwin package installs

--- imaging/scripts/provision-cygwin.ps1
+++ imaging/scripts/provision-cygwin.ps1
@@ -1,4 +1,6 @@
 # Cygwin and Cygwin packages
 choco install cygwin -y
-choco install bash --source cygwin -y
+
+# Cant get any of the --source cygwin package installs to work ...
+# choco install bash --source cygwin -y
 
