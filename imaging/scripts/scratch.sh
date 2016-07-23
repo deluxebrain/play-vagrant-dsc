4316d31d5ee1255c7b8892f8efefcfcdd116022a
deluxebrain <matt.caton@mac.com>
2016-07-23 18:57:44 +0100

    Continuing with ssh setup

--- imaging/scripts/scratch.sh
+++ imaging/scripts/scratch.sh
@@ -4,3 +4,6 @@ mkdir -p /home/Vagrant/.ssh
 
 wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/Vagrant/.ssh/authorized_keys
 
+# create a /etc/passwd file ( use the -l option to grab the user information from the local machine )
+mkpasswd -l > /etc/passwd
+
